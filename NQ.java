dd25c695327dd939c39685a4fa4150182227ed28
Dipankar debnath <68681679+dipankar108@users.noreply.github.com>
2022-01-03 20:43:52 +0600

    adding some comment for better understand code

--- NQ.java
+++ NQ.java
@@ -1,5 +1,13 @@
+//This is NQueen backtracking java code.
+// In this code default boasd initialized by 0. It means there is No queen.
+// If we are able to replace queen then we replaced 0 to 1.
+// you can use String for this operation but it needs some modification.
+
+
 public class NQ{
 	static boolean isSafe(int[][] board,int row, int col ){
+		//Here if all the position that checked if there is already queen then its value is already 1 so that means we cannot add any queen on that 
+		//position and we return false.
 		// check horizontali
 		for (int i=0; i<board.length; i++) {
 			if (board[row][i]==1)return false;
@@ -32,7 +40,9 @@ public class NQ{
 		for (int col=0;col<N ;col++ ) {
 			if (isSafe(board,row,col)) {
 			      board[row][col]=1;
+				// call recursion function
 				if (Nqueen(board,row+1))return true;
+				// if we got false than we nwwd to backtrack old queen position I mean make it 00 again 
 				   board[row][col]=0;
 			}
 		}
@@ -41,6 +51,7 @@ public class NQ{
 	public static void main(String[] args) {
 		int[][] board={{0,0,0,0},{0,0,0,0},{0,0,0,0},{0,0,0,0}};
 		Nqueen(board,0); 
+		//Printing new solution
 		for (int i=0; i<board.length;i++ ) {
 for (int j=0; j<board.length;j++ ) {
 				System.out.print(board[i][j]);
